ed217ba14ac61f0aa7b119228ee08e620c4a2f97
dasshubham762 <dasshubham762@pixelexperience.org>
2021-03-31 10:03:10 +0530

    Revert "Settings: Phone ringtone setting for Multi SIM device [2/3]"
    This reverts commit 4e0d9f51c52bb0857e243ca07f5cb15e28d83a6d.

--- src/com/android/settings/notification/RingtonePreferenceControllerBase.java
+++ src/com/android/settings/notification/RingtonePreferenceControllerBase.java
@@ -24,7 +24,6 @@ import android.util.Log;
 
 import androidx.preference.Preference;
 
-import com.android.settings.RingtonePreference;
 import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.utils.ThreadUtils;
@@ -52,8 +51,8 @@ public abstract class RingtonePreferenceControllerBase extends AbstractPreferenc
     }
 
     private void updateSummary(Preference preference) {
-        final Uri ringtoneUri = RingtoneManager.getActualDefaultRingtoneUriBySlot(mContext,
-                getRingtoneType(), ((RingtonePreference)preference).getSlotId());
+        final Uri ringtoneUri = RingtoneManager.getActualDefaultRingtoneUri(
+                mContext, getRingtoneType());
 
         final CharSequence summary;
         try {
